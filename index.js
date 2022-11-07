0283cb539b02d7bcadbf435212e64adcf2b92ba7
zahidupm <zahidhasanlimon111@gmail.com>
2022-11-07 22:27:26 +0600

    server is running

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Journalist service server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+})

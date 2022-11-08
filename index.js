167101c19f22d4d1250892be00f74a7093cc10f0
zahidupm <zahidhasanlimon111@gmail.com>
2022-11-08 14:20:51 +0600

    add middleware express.josn

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
+require('dotenv').config();
+require('colors');
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Journalist service server is running')
